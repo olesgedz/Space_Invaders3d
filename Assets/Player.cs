9d74b783b0be58af21e8926097e2cdf36e3ee725
olesgedz <olesgedz@rambler.ru>
2019-08-05 21:51:39 +0300

    first commit

---
+++ Assets/Player.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    // Start is called before the first frame update
+
+    public float speed;
+    public float tilt;
+    public float xMin, xMax, zMin, zMax;
+
+    public GameObject lazerShot;
+    public Transform gunPosition;
+
+    public float shotDelay;
+
+    private float nextShotTime;
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Time.time > nextShotTime)
+        {
+            Instantiate(lazerShot, gunPosition.position, Quaternion.identity);
+            nextShotTime = Time.time + shotDelay;
+        }
+       float moveHorizontal = Input.GetAxis("Horizontal");
+       float moveVertical = Input.GetAxis("Vertical");
+
+       Rigidbody Ship = GetComponent<Rigidbody>();
+       Ship.velocity = new Vector3(moveHorizontal, 0 , moveVertical) * speed;
+       Ship.rotation = Quaternion.Euler(-tilt * Ship.velocity.z, 0, -tilt * Ship.velocity.x);
+
+        float xPosition = Mathf.Clamp(Ship.position.x, xMin, xMax);
+        float zPosition = Mathf.Clamp(Ship.position.z, zMin, zMax);
+        Ship.position = new Vector3(xPosition, 0, zPosition);
+    }
+}
---
+++ Assets/ShotScript.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShotScript : MonoBehaviour
+{
+    public float speed;
+    void Start()
+    {
+        GetComponent<Rigidbody>().velocity = Vector3.forward * speed;
+    }
+
+    void  Update()
+    {
+        if (GetComponent<Rigidbody>().position.z > 30)
+            Destroy(gameObject); 
+    }
+}
