7fff7eee1d348c1f019b217c3585cd41c700135a
olesgedz <olesgedz@rambler.ru>
2019-08-06 21:12:18 +0300

    asteroid

---
+++ Assets/AsteroidScript.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AsteroidScript : MonoBehaviour
+{
+    // Start is called before the first frame update
+    
+    private Rigidbody Asteroid;
+    public float rotationSpeed;
+
+    public float minSpeed, maxSpeed;
+
+    public GameObject asteroidExplosion;
+    public GameObject playerExplosion;
+
+    void Start()
+    {
+        Asteroid = GetComponent<Rigidbody>();
+        Asteroid.angularVelocity = Random.insideUnitSphere * rotationSpeed;
+        Asteroid.velocity = Vector3.back * Random.Range(minSpeed, maxSpeed);
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "GameBoundry")
+        {
+            return;
+        }    
+        Instantiate(asteroidExplosion, transform.position, Quaternion.identity);
+        if (other.tag == "Player")
+        {
+          Instantiate(playerExplosion, other.transform.position, Quaternion.identity);
+        }
+        Destroy(other.gameObject);
+        Destroy(gameObject);
+    }
+
+    void Update()
+    {
+
+    }
+}
